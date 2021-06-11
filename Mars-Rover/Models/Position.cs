256ee351a7f8cb02ddb3955096be124c07587a41
surajvermav38 <surajvermav38@gmail.com>
2021-06-11 16:47:43 +0530

    add rover implementation

---
+++ Mars-Rover/Models/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mars_Rover
+{
+    class Position
+    {
+        public int xCoordinate { get; set; }
+        public int yCoordinate { get; set; }
+        public string direction { get; set; }
+
+    }
+}
---
+++ Mars-Rover/Models/Rover.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Mars_Rover
+{
+    public class Rover
+    {
+        Position position = new Position();
+        public void SetPosition(string positionString)
+        {
+            position.xCoordinate = (Convert.ToInt32(positionString.Split(',')[0]));
+            position.yCoordinate = (Convert.ToInt32(positionString.Split(',')[1]));
+            position.direction = (positionString.Split(',')[2]);
+        }
+
+        public string GetPosition()
+        {
+            return position.xCoordinate.ToString() + "," + position.yCoordinate.ToString() + "," + position.direction;
+        }
+
+        public void ProcessInstructions(string instruction)
+        {
+            for (int i = 0; i < instruction.Length; i++)
+            {
+                if (instruction[i] == 'M')
+                {
+                    Processor.MoveRoverForward(position);
+                }
+                else
+                {
+                    Processor.ChangeDirection(instruction[i], position);
+                }
+            }
+        }
+    }
+}
